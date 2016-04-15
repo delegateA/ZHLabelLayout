e2da35d7358ed5e26630ffefe4d0e641722cb22a
delegateB <zhihua.zhao@synwing.com>
2016-04-15 18:30:01 +0800

    add
    ADD demo

---
+++ ZHLabelView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHLabelView
+//
+//  Created by zzh_iPhone on 16/4/15.
+//  Copyright © 2016年 zzh_iPhone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
